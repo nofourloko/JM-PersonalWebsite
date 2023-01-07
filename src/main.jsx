0e08fd0b36be7353282e67be753a8d7ff627fadd
nofourloko <jakub.mista@icloud.com>
2023-01-07 20:30:08 +0100

    up

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,9 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import NewPage from './Context/newPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Router>
       <NewPage>
         <App />
       </NewPage>
     </Router>
-  </React.StrictMode>,
 )
